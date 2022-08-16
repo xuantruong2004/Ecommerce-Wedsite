7607562429a038182e27e5b815913fa45efc7dcc
xuantruong2004 <truongxuantp3@gmail.com>
2022-08-16 21:05:53 +0700

    update 16/08

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+const addCart = document.querySelectorAll(".pro-cart");
+
+// console.log(addCart);
+addCart.forEach((cart) => {
+  cart.addEventListener("click", (e) => {
+    let cartParent = cart.parentElement;
+    let img = cartParent.querySelector("img").src;
+    let brand = cartParent.querySelector("span").innerText;
+    let nameProduction = cartParent.querySelector("h5").innerText;
+    let price = cartParent.querySelector("h4").innerText;
+
+    cartList(img, brand, nameProduction, price);
+    e.preventDefault();
+  });
+});
+
+const cartList = (img, brand, nameProduction, price) => {
+  const elementTbody = document.querySelector("tbody");
+
+  const trAdd = document.createElement("tr");
+  trAdd.classList.add("tr");
+  const textHtml = `
+        <td><a href="#"> <i class="far fa-times-circle"></i></a></td>
+        <td><img src=${img} alt="" /></td>
+        <td>${nameProduction}</td>
+        <td>${price}</td>
+        <td><input type="number" value="1" /></td>
+        <td>${price}</td>`;
+  trAdd.innerHTML = textHtml;
+  console.log(trAdd);
+  elementTbody.appendChild(trAdd);
+};
